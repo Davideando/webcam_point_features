661444a2aa87599e8301b0695fcf1437ef3db285
Deivid1 <deivid@test.com>
2016-11-01 18:35:50 +0100

    Ejercicio 2.1

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # webcam_point_features
 Detection of ORB features from online webcam imges.
+
+La dirección donde se explican las funcionalidades del detector de puntos ORB (Oriented FAST and Rotated BRIEF)
+
+http://docs.opencv.org/trunk/db/d95/classcv_1_1ORB.html
+
+Referencia de la wikipedia
+
+https://en.wikipedia.org/wiki/ORB_(feature_descriptor)
+
+El paper que hablo por primera vez del algoritmo ORB
+
+http://www.vision.cs.chubu.ac.jp/CV-R/pdf/Rublee_iccv2011.pdf
+
+Explicación de los distintos tipos de filtros
+
+https://moodle2015-16.ua.es/moodle/pluginfile.php/105463/mod_page/content/43/3%20Caracteristicas%20y%20segmentacion.pdf
+
+Analisis de eficiencia de algoritmos de reconocimiento
+
+http://repositorio.espe.edu.ec/bitstream/21000/7770/1/AC-SI-ESPE-047430.pdf
+
+Un detector de esquinas ORB utiliza las caracteristicas de dos filtros, el filtro FAST y el filtro BRIEF, utilizando las ventajas 
+que proporciona el filtro BRIEF en fácilidad de computo y la detección del filtro Fast para mejorar el tiempo de computación.
+
+El filtro ORB utiliza un sistema de pixeles vecinos para detectar las esquinas, utiliza un algoritmo para detectar donde se producen estos 
+cambios en la imagen para prooponer las esquinas. Además de la detección de las esquinas, se utilizan los momentos para calcular la orientación del punto. Por otro lado, se calculan los descriptores de la imagen utilizando el filtro BRIEF, pero se ha mejorado con un calculo de pares, de tal manera que con este calculo se consigue que la detección sea inmune a la rotación, y por tanto, da igual donde esté apuntando, que detectará el punto.
+
+El código lo he hecho con una mascará configurable, pero no he notado que hayan más o menos detecciones cuando he aplicado el filtro, no se si he hecho alguna cosa mal...
+
--- src/point_features.cpp
+++ src/point_features.cpp
@@ -11,6 +11,7 @@
 
 //consts
 const unsigned int MIN_NUM_FEATURES = 300; //minimum number of point fetaures
+const unsigned int SAMPLE_PARTS = 4; // Numero de partes en la que dividirá la imagen en X y en Y
 
 int main(int argc, char *argv[]) 
 {
@@ -47,6 +48,10 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    // Se adquiere el tamaño de las imagenes que provienen de la cámara
+    int camera_width = camera.get(CV_CAP_PROP_FRAME_WIDTH);
+    int camera_height = camera.get(CV_CAP_PROP_FRAME_HEIGHT);
+
     //Process loop. Capture and point feature extraction. User can quit pressing a key
     while(1)
 	{
@@ -56,17 +61,40 @@ int main(int argc, char *argv[])
             std::cout << "No image" << std::endl;
             cv::waitKey();
         }
-        		
-    //**************** Find ORB point fetaures and descriptors ****************************
-        
-        //clear previous points
-        point_set.clear(); 
-        
-        //detect and compute(extract) features
-        orb_detector->detectAndCompute(image, cv::noArray(), point_set, descriptor_set);
-        
-        //draw points on the image
-        cv::drawKeypoints( image, point_set, image, 255, cv::DrawMatchesFlags::DEFAULT );      
+
+        // Se crea un bucle para recorrer todos los rincones de la imagen
+        // en este caso se dividirá la imagen por los trozos que indique la constante 
+        // SAMPLE_PARTS.
+        for(int i = 0; i < SAMPLE_PARTS; i++)
+        {
+        	for(int j = 0; j < SAMPLE_PARTS; j++)
+        	{
+		    //******************** Se define la máscara****************//
+
+		        // Se define una mascará de un canal de datos de 8 bits
+		        cv::Mat mascara = cv::Mat::zeros(image.size(), CV_8UC1);
+
+		        // Se define un area de interes, que será la mascara
+		        cv::Mat roi(mascara, cv::Rect(0+i*camera_width/SAMPLE_PARTS,0+j*camera_height/SAMPLE_PARTS,camera_width/SAMPLE_PARTS,camera_height/SAMPLE_PARTS));
+		        // Se colocan los bytes de la zona donde se quiere aplicar la máscara
+		        // a 1, y se deja el resto a 0.
+		        roi = cv::Scalar(255,255,255);
+		        // Como la máscara y el area de interes (roi) están enlazados, todos los cambios que se realizen en 1, se realizarán 
+		        // en el otro.
+
+		        		
+		    //**************** Find ORB point fetaures and descriptors ****************************
+		        
+		        //clear previous points
+		        point_set.clear(); 
+		        
+		        //detect and compute(extract) features
+		        orb_detector->detectAndCompute(image, mascara, point_set, descriptor_set);
+		        
+		        //draw points on the image
+		        cv::drawKeypoints( image, point_set, image, 255, cv::DrawMatchesFlags::DEFAULT );     
+		    }
+        } 
                 
     //********************************************************************
 		
